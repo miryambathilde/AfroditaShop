5560d9375835309ba8b1e9c41d2b43ae07911cfb
Miryam Bathilde <mbathilde@gmail.com>
2021-07-14 13:34:33 +0200

    datos conexion DB en archivo

--- PruebaConexionDB.js
+++ PruebaConexionDB.js
@@ -1,1 +1,13 @@
 const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'admin',
+	database: 'afrodita_shop',
+	port: 3306,
+});
+
+connection.connect((err) => {
+	console.log(err);
+});
