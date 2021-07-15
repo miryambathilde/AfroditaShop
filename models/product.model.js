5ed615fad56c05c3141e34572f55550b96710f16
Miryam Bathilde <mbathilde@gmail.com>
2021-07-15 13:05:03 +0200

    getById + create product

--- models/product.model.js
+++ models/product.model.js
@@ -12,8 +12,48 @@ const getAll = () => {
 	return prom;
 };
 
+// RECUPERAR EL PRODUCTO POR ID - select * from product where id = 1 //
+// el ? es para parametros dinnámicos y siempre despues de la ? tiene que ir un array con tantos valores como interrogaciones tenga la sentencia de la query
+// y después del array siempre va una función anómima con el error y el resultado, el resultado siempre será un array
+// o resuelve devolviendo un nulo, si no lo encuentra,  if (result.length !== 1) return resolve(null);
+// o se resuelve devolviendo el producto en sí resolve(result[0]);
+
+const getById = (productId) => {
+	return new Promise((resolve, reject) => {
+		db.query(
+			'select * from products where id = ?',
+			[productId],
+			(err, result) => {
+				if (err) return reject(err);
+				if (result.length !== 1) return resolve(null);
+				resolve(result[0]);
+			}
+		);
+	});
+};
+
+// insertar nuevo registro en la BBDD //
+// insert into productos (name, description, price, category, available, created_at) value (...)
+// el ? es para parametros dinnámicos y siempre despues de la ? tiene que ir un array con tantos valores como interrogaciones tenga la sentencia de la query
+// y después del array siempre va una función anómima con el error y el resultado, el resultado siempre será un array
+
+const create = ({name, description, price, category}) => {
+	return new Promise((resolve, reject) => {
+		db.query(
+			'insert into products (name, description, price, category, available, created_at) values(?, ?, ?, ?, ?, ?)',
+			[name, description, price, category, true, new Date()],
+			(err, result) => {
+				if (err) return reject(err);
+				resolve(result);
+			}
+		);
+	});
+};
+
 /*  exportamos el module/modelo y tiene una clave y valor, que si tienen el mismo nombre 
 puede ponerse solo una vez */
 module.exports = {
 	getAll,
+	getById,
+	create,
 };
--- routes/api/products.js
+++ routes/api/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // DONE: extraer solo las funciones que vamos a usar
-const { getAll } = require('../../models/product.model');
+const { getAll, getById, create } = require('../../models/product.model');
 
 router.get('/', (req, res) => {
 	// 1 - Recuperar los productos de la BBDD, como nos devuelve una promesa el metodo de la query, tenemos que poner el then y el catch
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 // Recuperar los productos de la BBDD, como nos devuelve una promesa el metodo de la query, tenemos que poner el then y el catch
-// TODO: CON opcion ASYNC AWAIT y TRY CATCH
+
 // 1. delante de la funcion que devuelve la promesa le ponemos el await
 // 2. el wait lo guardamos en una constante
 // 3. el async lo tenemos que ponerlo delante de la ejecución de la función
@@ -30,10 +30,59 @@ router.get('/v2', async (req, res) => {
 	}
 });
 
-// TODO: recuperar datos por ID
 
-router.post('/', (req, res) => {
-	res.end('Ruta POST /api/products');
+//como nos devuelve una promesa, tenemos que hacerlo con ASYNC AWAIT TRY CATCH
+
+router.get('/:productId', async (req, res) => {
+	try {
+	const result = await getById(req.params.productId);
+	if (result) {
+		res.json(result);
+	} else {
+		res.json({error: 'El producto no existe en la base de datos'})
+	}
+	} catch (error) {
+		res.json({ error: err.message });
+	}
+});
+
+// con opcion THEN CATCH, sin async ni await ni try catch, pero es lo mismo de antes
+router.get('/:productId/v2', (req, res) => {
+	getById(req.params.productId)
+			.then(result => {
+					if (result) {
+							res.json(result);
+					} else {
+							res.json({ error: 'El producto no existe en la base de datos' });
+					}
+			})
+			.catch(error => res.json({ error: error.message }));
+});
+
+// para añadir un nuevo producto //
+// con async await, el async siempre va delante del callback y después manejamos req y res dentro del try catch
+router.post('/', async (req, res) => {
+	try {
+		//INSERTAR los datos dentro de la BD //
+		const result = await create(req.body);
+		const product = await getById(result.insertId);
+		res.json(product);
+	} catch {
+		res.json({ error: error.message });
+	}
+});
+
+// con opcion THEN CATCH, sin async ni await ni try catch, pero es lo mismo de antes
+router.post('/v2', (req, res) => {
+	create(req.body)
+			.then(result => {
+					getById(result.insertId)
+							.then(product => {
+									res.json(product);
+							})
+							.catch(error => res.json({ error: error.message }));
+			})
+			.catch(error => res.json({ error: error.message }));
 });
 
 router.put('/', (req, res) => {
