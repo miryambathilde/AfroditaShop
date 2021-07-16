d993655751f5bbf6008351d9cb95b5d679b3f500
Miryam Bathilde <mbathilde@gmail.com>
2021-07-16 11:38:07 +0200

    refactoring helper methods and clean code files

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 /* HELPER METHODS */
 
 // hacemos una funcion para ejecutar las queries, pasamos por parametro, la query y el array de los parametros que es lo que cambia en cada ejecución
-function executeQuery(query, arrParams) {
+function executeQuery(query, arrParams = []) {
 	return new Promise((resolve, reject) => {
 		db.query(query, arrParams, (err, result) => {
 			if (err) return reject(err);
@@ -9,3 +9,7 @@ function executeQuery(query, arrParams) {
 		});
 	});
 }
+
+module.exports = {
+	executeQuery,
+};
--- models/client.model.js
+++ models/client.model.js
@@ -1,38 +1,14 @@
-// METODO #1 PARA OBTENER TODOS LOS CLIENTES //
+const { executeQuery } = require('../helpers');
 
-/* const getAll = () => {
-	return new Promise((resolve, reject) => {
-		db.query('select * from clients', (err, result) => {
-			if (err) return reject(err);
-			resolve(result);
-		});
-	});
-};
- */
+// METODO #1 PARA OBTENER TODOS LOS CLIENTES //
 
 // REFACTORIZACION CON EL HELPER METHOD DE LA FUNCION CREATE //
 const getAll = () => {
 	return executeQuery('select * from clients');
 };
-// METODO #2 PARA CREART UN CLIENTE y luego pasarlo a la BBDD //
-// insertar nuevo registro en la BBDD //
-// insert into productos (name, email, address, phone) values (...)
-// el ? es para parametros dinnámicos y siempre despues de la ? tiene que ir un array con tantos valores como interrogaciones tenga la sentencia de la query
-// y después del array siempre va una función anómima con el error y el resultado, el resultado siempre será un array
 
-/* const create = ({ name, email, address, phone }) => {
-	return new Promise((resolve, reject) => {
-		db.query(
-			'insert into clients (name, email, address, phone) values (?,?,?,?)',
-			[name, email, address, phone],
-			(err, result) => {
-				if (err) return reject(err);
-				resolve(result);
-			}
-		);
-	});
-};
- */
+// METODO #2 PARA CREART UN CLIENTE y luego pasarlo a la BBDD //
+
 // REFACTORIZACION CON EL HELPER METHOD DE LA FUNCION CREATE //
 const create = ({ name, email, address, phone }) => {
 	return executeQuery(
