6a64a6ec9408ddc162f7e80268d040baed1016d8
Miryam Bathilde <mbathilde@gmail.com>
2021-07-16 09:15:00 +0200

    import getAll to clients api + async await

--- routes/api/clients.js
+++ routes/api/clients.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {getAll} = require('../../models/client.model');
 
 
 // GET
-router.get('/', (req, res) => {
-  res.end('/api/clients GET');
+/* como devuelve una promesa ASYNC AWAIT TRY CATCH */
+// 1. delante de la funcion que devuelve la promesa le ponemos el await
+// 2. el await lo guardamos en una constante
+// 3. el async lo tenemos que ponerlo delante de la ejecución de la función
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await getAll();
+    res.json(result);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 });
 
 
