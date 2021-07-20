7757402e5d11c562097bc6efa2fb9ec956531a4c
Miryam Bathilde <mbathilde@gmail.com>
2021-07-20 10:39:03 +0200

    vista index.pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'Aprendiendo node con Mario',
+		clasesV2: ['primera', 'segunda', 'tercera'],
+		products: 1,
+		animals: ['perro', 'gato', 'hamster', 'conejo'],
+	});
 });
 
 module.exports = router;
