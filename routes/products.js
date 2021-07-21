fb0cedd3c6ad5fbd3a93856c063f2d0c468fa1ac
Miryam Bathilde <mbathilde@gmail.com>
2021-07-21 10:58:04 +0200

    product/update

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAll, create, getById } = require('../models/product.model');
+const { getAll, create, getById, update } = require('../models/product.model');
 
 router.get('/', (req, res) => {
 	// 1. Recuperar todos los productos de la BD - HECHO
@@ -25,6 +25,7 @@ VISTA: products/edit.pug
 	2. Renderizar la vista y pasarle el producto
 	3. Generar el formulario dentro de la vista:
 -Dentro del formulario debemos incorporar los datos del producto a editar */
+// en res.render, pasamos la vista y el producto a la vista //
 router.get('/edit/:productId', (req, res) => {
 	getById(req.params.productId)
 		.then((product) => {
@@ -32,6 +33,18 @@ router.get('/edit/:productId', (req, res) => {
 			res.render('products/edit', { product });
 		})
 		.catch((error) => console.log(error));
+});
+
+// POST localhost:3000/products/update
+// que vamos a hacer aqui? ejecutar metodo update del modelo
+// Que datos tengo disponibles? req.body(name, description, price, category, productId)
+// Que datos necesito? {name, description, price, category} + id
+router.post('/update', (req, res) => {
+	update(req.body.productId, req.body)
+		.then((result) => res.redirect('/products'))
+		.catch((error) => console.log(error));
+
+	//res.send('Formulario edit funciona');
 });
 
 router.get('/remove', (req, res) => {
